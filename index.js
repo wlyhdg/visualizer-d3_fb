f63f11cfec95b24d3bfed2dac37ce43c61799f58
Wael Yehdego <waelwc3@gmail.com>
2020-03-24 16:03:12 -0700

    create bar graph successfully using json data

--- index.js
+++ index.js
@@ -1,22 +1,74 @@
 // select svg container
-const svg = d3.select('svg');
+const svg = d3.select('.canvas')
+  .append('svg')
+  .attr('width', 600)
+  .attr('height', 600);
 
-d3.json('planets.json')
+const margin = {top: 20, right: 20, bottom: 100, left: 100};
+const graphWidth = 600 - margin.left - margin.right;
+const graphHeight = 600 - margin.top - margin.bottom;
+
+const graph = svg.append('g')
+  .attr('width', graphWidth)
+  .attr('height', graphHeight)
+  .attr('transform', `translate(${margin.left}, ${margin.top})`)
+
+// create the svg groups for graph axes
+const xAxisGroup = graph.append('g')
+  .attr('transform', `translate(0, ${graphHeight})`);
+
+const yAxisGroup = graph.append('g');
+
+d3.json('menu.json')
   .then(data => {
-    const circs = svg.selectAll('circle')
+    // add linear scale for y-axis
+    const y = d3.scaleLinear()
+    .domain([0, d3.max(data, d => d.orders)])
+    .range([graphHeight, 0]);
+
+    // const min = d3.min(data, d => d.orders)
+    // const max = d3.max(data, d => d.orders)
+    // const extent = d3.extent(data, d => d.orders)
+
+    // console.log(min, max, extent)
+
+    // add band scale for x-axis
+    const x = d3.scaleBand()
+      .domain(data.map(item => item.name))
+      .range([0,500])
+      .paddingInner(0.2)
+      .paddingOuter(0.2)
+
+    // join the data to rects
+    const rects = graph.selectAll('rect')
       .data(data);
 
-    // add circs to data already in DOM
-    circs.attr('cy', 200)
-      .attr('cx', d => d.distance)
-      .attr('r', d => d.radius)
-      .attr('fill', d => d.fill);
+    rects.attr('width', x.bandwidth)
+      .attr('height', d => graphHeight - y(d.orders))
+      .attr('fill', 'yellowgreen')
+      .attr('x', (d, i, n) => x(d.name))
+      .attr('y', d => y(d.orders));
 
     // append the enter selection to the DOM
-    circs.enter()
-      .append('circle')
-      .attr('cy', 200)
-      .attr('cx', d => d.distance)
-      .attr('r', d => d.radius)
-      .attr('fill', d => d.fill);
+    rects.enter()
+      .append('rect')
+      .attr('width', x.bandwidth)
+      .attr('height', d => graphHeight - y(d.orders))
+      .attr('fill', 'yellowgreen')
+      .attr('x', (d, i, n) => x(d.name))
+      .attr('y', d => y(d.orders));
+
+    // create and call the axes
+    const xAxis = d3.axisBottom(x);
+    const yAxis = d3.axisLeft(y)
+      .ticks(3)
+      .tickFormat(d => d + ' orders')
+
+    xAxisGroup.call(xAxis);
+    yAxisGroup.call(yAxis);
+
+    xAxisGroup.selectAll('text')
+      .attr('transform', 'rotate(-40)')
+      .attr('text-anchor', 'end')
+      .attr('fill', 'yellowgreen')
   })
